d9924eb88996f27678bb4f7902b8c588a026b1ff
Matheus Sehnem <matheusrnk@debian-rnk-machine.matheusrnk>
2024-06-21 14:52:12 -0300

    fourth commit

--- env_tools.sh
+++ env_tools.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-# Instala iperf3
 sudo apt install -y iperf3
 
-# Verifica se o ping está instalado (normalmente já está)
 if ! command -v ping &> /dev/null
 then
     echo "Ping não encontrado, algo está errado, por favor verifique."
 fi
 
-# Instala tshark
 sudo apt install -y tshark
 
-# Instala ftp
 sudo apt install -y ftp
 
-# Instala Python 3
 sudo apt install -y python3
 
-# Instala pip
 sudo apt install -y python3-pip
 
-# Instala dependências para o ryu-manager
 sudo apt install -y python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev zlib1g-dev
 
-# Instala ryu-manager usando pip
 pip3 install ryu
 
 mv mininet-test mininet/
 
-# Confirmação de instalação
 echo "Todas as ferramentas foram instaladas com sucesso!"
